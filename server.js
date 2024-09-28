06a6e416f15a1a24060d0b5021dd5b4b919e83f9
sreyesg <santiagoreyes.g@gmail.com>
2024-09-27 19:53:37 -0700

    require node packages to server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+// =============== require packages =================== //
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+
+// =============== require controllers =================== //
+
+
+
+
+// =============== middleware =================== //
+
+
+
+
+
+
+
+// =============== routes =================== //
+
+
+
+
+
+// =============== app listener =================== //
+app.listen(4000, (req, res) => {
+    console.log('listening on port 3000')
+})
