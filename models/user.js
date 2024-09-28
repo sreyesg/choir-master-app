763f64fdedb3814cdc17e4815e739e8a9ed1c496
sreyesg <santiagoreyes.g@gmail.com>
2024-09-27 20:16:49 -0700

    Add model schema and update collection view folders name to repertoire

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const repertoireSchema = new mongooseSchema({
+    title: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        enum: ['entrance','offertory','communion','exit']
+    },
+    songBook: {
+        type: String
+    },
+    page: {
+        type: String
+    }
+})
+
+const userSchema = mongoose.Schema({
+    username: { type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String},
+    choir: {type: String},
+    repertoire: [repertoireSchema] 
+})
+
+const User = mongoose.model ('User', userSchema)
+
+module.exports = User
+
