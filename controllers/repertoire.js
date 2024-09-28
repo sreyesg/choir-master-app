c166cb9f45583411a6b55609536604c6e2553ab7
sreyesg <santiagoreyes.g@gmail.com>
2024-09-28 14:22:59 -0700

    add create new song and render new form to repertoire controller

--- controllers/repertoire.js
+++ controllers/repertoire.js
@@ -3,17 +3,33 @@ const router = express.Router()
 const User = require('../models/user.js')
 
 // =========== render Index ============== //
-router.get('/',(req,res) => {
-    res.send('Index page')
+router.get('/',async(req,res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        res.render('repertoire/index.ejs', {
+            repertoire: currentUser.repertoire
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+    
 })
 
-
-
-
-
-
-
 // =========== send new form ============== //
+router.get('/new', (req, res) => {
+    res.render('repertoire/new.ejs')
+})
+// ============ Create repertoire ========= //
+router.post('/', async(req, res) => {
+    console.log(req.body)
+    const currentUser = await User.findById(req.session.user._id)
+    currentUser.repertoire.push(req.body)
+    currentUser.save()
+    res.redirect(`/users/${req.session.user._id}/repertoire`)
+})
+
 
 
 // =========== send edit form ============== //
